d965339eef031d3f9be0912c0a245a48ee2be4f2
AndersonPS <dson_adn@hotmail.com>
2025-06-21 20:46:41 -0300

    feat: logica da tela de login adicionada, programador da tela 2 deve inseria a logica da sua tela aqui e icone personalizado adicionado ao projeto

--- app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
+++ app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
@@ -5,12 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetoifoodgithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        logarUsuario()
+        cadastrarUsuario()
+
+    }
+
+    private fun logarUsuario() {
+        binding.editLoginEmail.text
+        binding.editLoginSenha.text
+
+        //ADICIONAR A LOGICA DO LOGIN AQUI
+        binding.btnLogin.setOnClickListener {}
+    }
+
+    //ADICIONAR A LOGICA DO CADASTRO AQUI, CASO FEITA
+    private fun cadastrarUsuario() {
+        binding.textCadastro.setOnClickListener {}
     }
 }
