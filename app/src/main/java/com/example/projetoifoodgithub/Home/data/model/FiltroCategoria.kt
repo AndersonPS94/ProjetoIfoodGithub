b399f908955247867f4647fd806caeca621b261c
AndersonPS <dson_adn@hotmail.com>
2025-06-22 14:46:00 -0300

    feat,style: criacao da tela home apos o login e a utilizacao de componentes de estilo para a melhor aparencia do app

---
+++ app/src/main/java/com/example/projetoifoodgithub/Home/data/model/FiltroCategoria.kt
@@ -0,0 +1,6 @@
+package com.example.aulaifood.domain.model
+
+data class FiltroCategoria(
+    val nome: String,
+    val url: String
+)
---
+++ app/src/main/java/com/example/projetoifoodgithub/Home/data/model/Loja.kt
@@ -0,0 +1,7 @@
+package com.example.aulaifood.domain.model
+
+data class Loja(
+    val nome: String,
+    val categoria: String,
+    val urlImagem: String
+)
