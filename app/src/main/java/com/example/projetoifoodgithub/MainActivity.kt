ae55432c3d9a40de1b3d4a46ac91fdd8ce379a16
AndersonPS <dson_adn@hotmail.com>
2025-06-21 21:20:19 -0300

    fix: correção da logica da tela login, erro corrigido

--- app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
+++ app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.projetoifoodgithub
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.projetoifoodgithub.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
