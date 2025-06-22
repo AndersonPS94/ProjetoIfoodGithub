6fc3e69e1d5797488b8d5c6615e86113f78922b5
Osmar F Cavalcante <osmarnube@gmail.com>
2025-06-22 11:07:17 -0300

    feat: implementar tela 5 de confirmação de pedido

--- app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
+++ app/src/main/java/com/example/projetoifoodgithub/MainActivity.kt
@@ -1,33 +1,34 @@
 package com.example.projetoifoodgithub
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.example.projetoifoodgithub.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        logarUsuario()
+        val btnVoltar = findViewById<ImageView>(R.id.btnVoltar)
+        val txtResumo = findViewById<TextView>(R.id.txtResumo)
+        val btnAdicionarPagamento = findViewById<Button>(R.id.btnAdicionarPagamento)
+        val btnConfirmar = findViewById<Button>(R.id.btnConfirmar)
+
+        btnVoltar.setOnClickListener {
+
+        }
+
+
+        txtResumo.text = """ Resumo dos valores   Subtotal: R$ 100,00 Taxa de entrega: R$ 6,90 Total: R$ 106,90""".trimIndent()
+
+        btnAdicionarPagamento.setOnClickListener {
+
+        }
+
+
+        btnConfirmar.setOnClickListener {
+
+        }
     }
-
-    private fun logarUsuario() {
-        binding.editLoginEmail.text
-        binding.editLoginSenha.text
-
-        //adicionar a logica da tela da loja pelo usuario 2 aqui
-        binding.btnLogin.setOnClickListener {}
-    }
-
-
 }
