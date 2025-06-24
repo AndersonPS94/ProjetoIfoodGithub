19cb26980881dd6f394ba03245c38e478c03e1c2
Reginaldo de Lima <bigrege@hotmail.com>
2025-06-24 15:44:57 -0300

    codigo inicial de  detalhes

--- app/src/main/java/com/example/projetoifoodgithub/DetalhesRestauranteActivity.kt
+++ app/src/main/java/com/example/projetoifoodgithub/DetalhesRestauranteActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetoifoodgithub.databinding.ActivityDetalhesRestauranteBinding
 
 class DetalhesRestauranteActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityDetalhesRestauranteBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalhes_restaurante)
+        setContentView(binding.root)
+        with(binding){
+            shapeImagePedido.setOnClickListener {
+
+
+            }
+
+        }
 
     }
 }
