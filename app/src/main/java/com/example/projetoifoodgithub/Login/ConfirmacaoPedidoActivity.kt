c2c85dcf64f6d04bab47a0faa9093fa664d4fdcc
Osmar F Cavalcante <osmarnube@gmail.com>
2025-06-26 21:36:47 -0300

    feat: adicionar tela 5 com layout e imagens

---
+++ app/src/main/java/com/example/projetoifoodgithub/Login/ConfirmacaoPedidoActivity.kt
@@ -0,0 +1,33 @@
+package com.example.projetoifoodgithub.Login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetoifoodgithub.databinding.ActivityConfirmacaoPedidoBinding
+
+class ConfirmacaoPedidoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfirmacaoPedidoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfirmacaoPedidoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltar.setOnClickListener {
+            finish()
+        }
+
+        binding.txtResumo.text = """Resumo dos valores
+Subtotal: R$ 100,00
+Taxa de entrega: R$ 6,90
+Total: R$ 106,90""".trimIndent()
+
+        binding.btnAdicionarPagamento.setOnClickListener {
+
+        }
+
+        binding.btnConfirmar.setOnClickListener {
+
+        }
+    }
+}
